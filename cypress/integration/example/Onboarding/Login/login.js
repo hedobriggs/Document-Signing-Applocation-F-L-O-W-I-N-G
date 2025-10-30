8ec76e9ecb5f7a326bfea0f859b81637e78540c8
hedobriggs <79212477+hedobriggs@users.noreply.github.com>
2025-10-30 11:39:55 +0000

    Update Login information

--- cypress/integration/example/Onboarding/Login/login.js
+++ cypress/integration/example/Onboarding/Login/login.js
@@ -11,10 +11,10 @@ describe('Flowmono', function()
         cy.get('.me-lg-3 > .mat-ripple').click() //clicks on the sign in button
         
         //enter email address
-        cy.get(':nth-child(1) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type("flowlive@yopmail.com")
+        cy.get(':nth-child(1) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type("flying@wings.com")
         
         // enter password
-        cy.get(':nth-child(2) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type("Password@101") 
+        cy.get(':nth-child(2) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type("Password") 
         
         //Click on the login button
         cy.get('.w-100 > .btn').click()
